84bc6e35ba1af630cffc952b9e416acfb41c7fe2
LaraKra <konlor@rambler.ru>
2022-05-07 23:57:45 +0300

    add test Money

--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -1,4 +1,39 @@
 package ru.netology.test;
 
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.DashboardPage;
+import ru.netology.page.LoginPage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class MoneyTransferTest {
-}
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:9999/");
+        val loginPage = new LoginPage();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationInfo = DataHelper.getVerificationCode(authInfo);
+        val dashboardPage = verificationPage.validVerify(verificationInfo);
+    }
+    @Test
+    void shouldTransferMoneyFromFirst() {
+
+        val dashboardPage = new DashboardPage();
+        int amount = 500;
+        val balanceFirstResult = dashboardPage.getFirstCardBalance() - amount;
+        val balanceSecondResult = dashboardPage.getSecondCardBalance() + amount;
+        val transferPage = dashboardPage.secondCardBalance();
+        transferPage.actionTransfer(DataHelper.getFirstCardNumber(), amount);
+        val actualFirstResult = dashboardPage.getFirstCardBalance();
+        val actualSecondResult = dashboardPage.getSecondCardBalance();
+        assertEquals(balanceFirstResult, actualFirstResult);
+        assertEquals(balanceSecondResult, actualSecondResult);
+    }
+
+    }
